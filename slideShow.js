473b972399806c275a0d2213982a878ab7e0023a
Bernard Telles <bernardtelgon@gmail.com>
2020-11-06 00:59:42 -0300

    slides

---
+++ slideShow.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const images = [
+  {'id': '1', 'url': '/img/chrono.jpg'}
+  {'id': '2', 'url': '/img/inuyasha.jpg'}
+  {'id': '3', 'url': '/img/ippo.jpg'}
+  {'id': '4', 'url': '/img/tenchi.jpg'}
+  {'id': '5', 'url': '/img/yuyuhakusho.jpg'}
+]
+
+
+const container = document.querySelector('#container-items');
+
+const loadImages = ( images, container ) => {
+    images.array.forEach(element => {
+      container.innerHTML = 
+    });
+}
+
+
+loadImages( images, container );
