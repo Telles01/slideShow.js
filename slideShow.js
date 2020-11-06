f776900db551c422fcbaec7386b4467f290279b3
Bernard Telles <bernardtelgon@gmail.com>
2020-11-06 01:21:44 -0300

    slide show

--- slideShow.js
+++ slideShow.js
@@ -1,20 +1,23 @@
 'use strict';
 
 const images = [
-  {'id': '1', 'url': '/img/chrono.jpg'}
-  {'id': '2', 'url': '/img/inuyasha.jpg'}
-  {'id': '3', 'url': '/img/ippo.jpg'}
-  {'id': '4', 'url': '/img/tenchi.jpg'}
-  {'id': '5', 'url': '/img/yuyuhakusho.jpg'}
+  {'id': '1', 'url': '/img/chrono.jpg'},
+  {'id': '2', 'url': '/img/inuyasha.jpg'},
+  {'id': '3', 'url': '/img/ippo.png'},
+  {'id': '4', 'url': '/img/tenchi.jpg'},
+  {'id': '5', 'url': '/img/yuyuhakusho.jpg'},
 ]
 
 
 const container = document.querySelector('#container-items');
 
 const loadImages = ( images, container ) => {
-    images.array.forEach(element => {
-      container.innerHTML = 
-    });
+    images.forEach(image => {
+      container.innerHTML += 
+        <div class= 'item'>
+        <img src = '${image.url}'
+        </div>
+    })
 }
 
 
